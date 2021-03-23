f0b5369c399613ba23018256eb15b1279ba753df
unknown <lenovo@DESKTOP-QUDQCTV.(none)>
2021-03-23 11:16:40 +0100

    first commit

---
+++ src/main/java/net/learning/springboot/SpringbootBackendUsersManagementApplication.java
@@ -0,0 +1,33 @@
+package net.learning.springboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import net.learning.springboot.model.User;
+import net.learning.springboot.repository.UserRepository;
+
+
+@SpringBootApplication
+public class SpringbootBackendUsersManagementApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendUsersManagementApplication.class, args);
+	}
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception{
+ 		this.userRepository.save(new User("Ouafae", "Ouhadi", "ouafae@gmail.com"));
+ 		this.userRepository.save(new User("Mohamed", "Mezgouri", "mohamed@gmail.com"));		
+ 		this.userRepository.save(new User("Hafsa", "Mahboub", "hafsa@gmail.com"));		
+ 
+	}
+
+
+
+}
+
---
+++ src/test/java/net/learning/springboot/SpringbootBackendUsersManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package net.learning.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBackendUsersManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
